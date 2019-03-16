5f04f53992abb8595c804b988abace2868272384
Erica Bista <ericabista77@gmail.com>
2019-03-16 18:21:31 +1100

    added collision detection feature

--- script.js
+++ script.js
@@ -10,9 +10,42 @@ var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth)/2;
 var rightPress = false;
 var leftPress = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeigth =  20;
+var brickPadding = 10;
+var brickOffsetLeft = 30;
+var brickOffsetTop = 30;
+
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+	bricks[c] = [];
+	for(r=0; r<brickRowCount; r++) {
+		bricks[c][r] =  {x: 0, y: 0, status: 1}
+	}
+}
 
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
+
+function drawBricks() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			if (bricks[c][r].status == 1) {
+				var brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft;
+				var brickY = (r*(brickHeigth + brickPadding)) + brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeigth);
+				ctx.fillStyle = '#0095DD';
+				ctx.fill();
+				ctx.closePath();
+			}
+		}
+	}
+}
 
 function keyDownHandler(e) {
 	if(e.keyCode == 39) {
@@ -48,10 +81,26 @@ function drawPaddle() {
 	ctx.closePath();
 }
 
+function collisionDetection() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var b = bricks[c][r];
+			if (b.status == 1) {
+				if (x > b.x && x < b.c + brickWidth && y > b.y && y < b.y + brickHeigth ) {
+					dy = -dy;
+					b.status = 0;
+				}
+			}
+		}
+	}
+}
+
 function draw() {
 	ctx.clearRect(0,0, canvas.width, canvas.height);
+	drawBricks();
 	drawBall();
 	drawPaddle();
+	collisionDetection();
 
 	if(y + dy < ballRadius) {
 		dy = -dy;
@@ -61,7 +110,7 @@ function draw() {
 			dy = -dy;
 		} else {
 			alert("GAME OVER!!!");
-			window.location.reload();
+			document.location.reload();
 		}
 	}
 	if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
